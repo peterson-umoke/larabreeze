ca22f1779e588439e4dfaa6971a08d175f76b8e2
Peterson Umoke <umoke10@hotmail.com>
2020-12-04 21:49:36 +0100

    making sure when a user logs out, the admin is not logged out

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -45,9 +45,9 @@ class AuthenticatedSessionController extends Controller
     {
         Auth::logout();
 
-        $request->session()->invalidate();
+//        $request->session()->invalidate();
 
-        $request->session()->regenerateToken();
+//        $request->session()->regenerateToken();
 
         return redirect('/');
     }
