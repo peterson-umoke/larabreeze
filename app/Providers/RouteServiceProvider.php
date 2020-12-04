78611805c2c6abb7fde2e3b80252ed470c5c2ddb
Peterson Umoke <umoke10@hotmail.com>
2020-12-04 21:51:18 +0100

    set the home for the admin

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
+    public const HOME_ADMIN = '/admin/dashboard';
 
     /**
      * The controller namespace for the application.
