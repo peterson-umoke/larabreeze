f0d1d42229a696030acb2352c625f070c2de481f
Peterson Umoke <umoke10@hotmail.com>
2020-12-04 21:30:09 +0100

    updating the routes for the admin

--- routes/web.admin.php
+++ routes/web.admin.php
@@ -8,5 +8,4 @@ Route::prefix('admin')->name("admin.")->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->middleware(['auth'])->name('dashboard');
-
 });
