758ccb3e00f3c397366866ed65dde8c08d5f5422
Peterson Umoke <umoke10@hotmail.com>
2020-12-04 23:37:19 +0100

    fixng thigns

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,17 +3,18 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param array $guards
+     * @param Request $request
+     * @param mixed ...$guards
      * @return string|null
      */
-    protected function redirectTo($request, array $guards)
+    protected function redirectTo($request, ... $guards)
     {
         if (!$request->expectsJson()) {
             switch (current($guards)):
