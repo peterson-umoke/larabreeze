686429e93ed5f6d1ca5e82f33a0ac08ca41c7ba0
Peterson Umoke <umoke10@hotmail.com>
2020-12-05 01:35:20 +0100

    complete the route

--- routes/web/user.web.php
+++ routes/web/user.web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('user.dashboard');
 })->middleware(['auth'])->name('dashboard');
