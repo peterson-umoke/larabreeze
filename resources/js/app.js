e7183b009ac3493619c8310628f54e48f592d069
Peterson Umoke <umoke10@hotmail.com>
2020-12-05 17:33:55 +0100

    still setting things up

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,8 @@
 require('./bootstrap');
-
-// require('alpinejs');
-
 require('moment');
-
-import { App, plugin } from '@inertiajs/inertia-vue'
+// require('alpinejs');
+import {InertiaProgress} from "@inertiajs/progress";
+import {App, plugin} from '@inertiajs/inertia-vue'
 import Vue from 'vue'
 
 Vue.use(plugin)
@@ -15,8 +13,8 @@ new Vue({
     render: h => h(App, {
         props: {
             initialPage: JSON.parse(el.dataset.page),
-            resolveComponent: name => require(`./Pages/${name}`).default,
+            resolveComponent: name => import(`./Pages/${name}`).then(module => module.default),
         },
     }),
 }).$mount(el)
-
+InertiaProgress.init()
