4f2f49216341f8658e6814db1ab68ea3923b948a
Peterson Umoke <umoke10@hotmail.com>
2020-12-05 17:26:27 +0100

    still setting up inertiajs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,23 @@
 require('./bootstrap');
 
-require('alpinejs');
+// require('alpinejs');
+require('moment');
+
+import Vue from 'vue';
+
+Vue.mixin({ methods: { route } });
+Vue.use(InertiaApp);
+Vue.use(InertiaForm);
+Vue.use(PortalVue);
+
+const app = document.getElementById('app');
+
+new Vue({
+    render: (h) =>
+        h(InertiaApp, {
+            props: {
+                initialPage: JSON.parse(app.dataset.page),
+                resolveComponent: (name) => require(`./Pages/${name}`).default,
+            },
+        }),
+}).$mount(app);
